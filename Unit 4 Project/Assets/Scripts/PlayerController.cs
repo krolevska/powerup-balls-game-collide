b3bb84f587f1927ec36f2251bde620c5743532b6
krolevska <krolevska@gmail.com>
2023-08-08 13:03:05 +0300

    Game update

---
+++ Unit 4 Project/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody playerRb;
+    public float speed = 10.0f;
+    public bool hasPowerup = false;
+    private float powerupStrength = 15.0f;
+
+    private GameObject focalPoint;
+    public GameObject powerIndicator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+        focalPoint = GameObject.Find("FocalPoint");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float forwardInput = Input.GetAxis("Vertical");
+        playerRb.AddForce(focalPoint.transform.forward * forwardInput * speed);
+
+        powerIndicator.transform.position = playerRb.transform.position + new Vector3(0f,-0.5f, 0f);
+    }
+
+    IEnumerator PowerupCountdownRoutine ()
+    {
+        yield return new WaitForSeconds(7);
+        hasPowerup = false;
+        powerIndicator.SetActive(false);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("PowerUp"))
+        {
+            Destroy(other.gameObject);
+            hasPowerup = true;
+            StartCoroutine(PowerupCountdownRoutine());
+            powerIndicator.SetActive(true);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy") && hasPowerup)
+        {
+            Rigidbody enemyRb = collision.gameObject.GetComponent<Rigidbody>();
+            Vector3 awayFromPlayer = collision.gameObject.transform.position - transform.position;
+            enemyRb.AddForce(awayFromPlayer * powerupStrength, ForceMode.Impulse);
+            Debug.Log("Collided with: " + collision.gameObject.name + " with powerup set to " + hasPowerup);
+        }
+    }
+}
